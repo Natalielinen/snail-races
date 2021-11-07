06ec63ea9592f7c49f43aa321479a01f2055e32d
Natalie <nataliech0409@gmail.com>
2021-11-07 09:04:46 +0300

    еще небольшой рефакторинг и форматирование кода

--- index.js
+++ index.js
@@ -7,39 +7,9 @@ class Snail {
         this.selector = document.querySelector(`#${options.selector}`)
     }
 
-    move () {
+    move() {
         this.selector.style.transform = `translateX(${this.points}px)`
-
     }
-
-}
-
-function getWinner() {
-    let competitors = [
-        {
-            name: bob.name,
-            points: bob.points
-        },
-        {
-            name: garry.name,
-            points: garry.points
-        },
-        {
-            name: turbo.name,
-            points: turbo.points
-        },
-    ]
-
-   let max = Math.max(bob.points, garry.points, turbo.points)
-    let winner = '';
-    competitors.forEach(c => {
-        if(c.points === max) {
-            winner =  `The winner is ${c.name} with ${c.points} points`
-        }
-    })
-
-  return winner;
-
 }
 
 const garry = new Snail({
@@ -62,18 +32,42 @@ const turbo = new Snail({
     mainColor: 'blue',
     selector: 'turbo'
 })
+
+function getWinner() {
+    let competitors = [
+        {
+            name: bob.name,
+            points: bob.points
+        },
+        {
+            name: garry.name,
+            points: garry.points
+        },
+        {
+            name: turbo.name,
+            points: turbo.points
+        },
+    ]
+
+    let max = Math.max(bob.points, garry.points, turbo.points)
+    let winner = '';
+    competitors.forEach(c => {
+        if (c.points === max) {
+            winner = `The winner is ${c.name} with ${c.points} points`
+        }
+    })
+
+    return winner;
+
+}
+
 const startBtn = document.querySelector('.start-btn')
 const resetBtn = document.querySelector('.reset-btn')
 const winner = document.querySelector('.winner')
-const bobPointsSpan = document.querySelector('.bob-points')
-const garryPointsSpan = document.querySelector('.garry-points')
-const turboPointsSpan = document.querySelector('.turbo-points')
-
 
 startBtn.addEventListener('click', () => {
-    garry.move()
-    bob.move()
-    turbo.move()
+    let snails = [garry, bob, turbo]
+    snails.forEach(s => s.move())
     startBtn.disabled = true
     resetBtn.disabled = false
     setTimeout(() => winner.innerHTML = getWinner(), 1000)
